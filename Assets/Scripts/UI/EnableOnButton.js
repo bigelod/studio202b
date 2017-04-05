8c6bcace51add9483d46dfa57368511f2b99f30d
Daven Bigelow <bigelod@mcmaster.ca>
2017-04-05 07:47:28 -0400

    Fixed one more time? Also yay joypads!

---
+++ Assets/Scripts/UI/EnableOnButton.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+var enableObj : GameObject;
+
+var button = "Submit";
+
+//Simple script to enable START button to work
+
+function Start () {
+	
+}
+
+function Update () {
+    if (Input.GetButtonDown(button)) {
+        enableObj.SetActive(true);
+    }
+}
